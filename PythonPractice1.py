accfcfbc0c530e41ee197c02b4ce997d60e66628
Joshua T <joshuataggart12@gmail.com>
2022-08-30 16:00:03 -0400

    Update PythonPractice1.py

--- PythonPractice1.py
+++ PythonPractice1.py
@@ -8,9 +8,10 @@ x = 4 #int type variable, no declaration
 y = int(4) #casting variable type
 print(type(y)) #prints data type (in this case int)
 
-j, k, l = 4, 1, 3 #multiple variables at once
+j, k, l = 4, 1, 3 #multiple variables at once (change)
 
 print(j)
 
 
-#change
+
+#another change
